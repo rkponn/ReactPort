5aa5cd6608d99c0cedbdb9a77409acfb725bfc8a
Rock Ponn <ratannak.ponn@outlook.com>
2024-05-21 20:27:44 -0500

    Adding initial files for portfolio - About - Contact - Navbar - Tailwind - Vite - Eslint - Prettier - React
    TODO:
    Add projects with images
    Add animations? (GSAP)

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './features/navbar/Navbar';
+import About from './features/about-me/About';
+import Contact from './features/contact/Contact';
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <About />
+      <Contact />
+    </>
+  )
+}
